0d09bcdec630dc88ece7a34babbf974996bdb07a
LuizGFF <luizferreiraprojetos@gmail.com>
2024-02-27 14:26:28 -0300

    Backend de Categorias

---
+++ src/api/category/category.model.js
@@ -0,0 +1,22 @@
+import { DataTypes } from 'sequelize'
+
+import sequelize from '../../config/sequelize.js'
+
+const categoryModel = sequelize.define('categories', {
+  category_id: {
+    type: DataTypes.NUMBER,
+    primaryKey: true
+  },
+  category_name: {
+    type: DataTypes.STRING(100),
+    required: true,
+    allowNull: false,
+  },
+  sub_category_id: {
+    type: DataTypes.STRING(100),
+    required: true,
+    allowNull: false,
+  },
+})
+
+export default categoryModel

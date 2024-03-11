d15a1f338a2815770c3bd376506b15a655fc644e
LGFFProj <40917176+LGFFProj@users.noreply.github.com>
2024-03-11 20:53:10 -0300

    subcategory.dao.js, escrevendo

--- src/api/subcategory/subcategory.dao.js
+++ src/api/subcategory/subcategory.dao.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize'
-
+import Op from Sequelize;
 import subcategory from './subcategory.model.js'
 
 import { isDefined } from '../../helpers/object-helper.js'
@@ -9,74 +9,41 @@ const insertSubcategory = async data => {
   return await subcategory.create(data)
 }
 
-const findAndCountExamples = async params => {
-  const { limit, offset, sortBy, sortOrder, filters, search } = params
-
-  const options = {
+const findSubcategoryByID = async subcategoryID => {
+  return await subcategory.findOne({
     where: {
-      deletedAt: null,
-      ...(isDefined(search) && {
-        [Sequelize.Op.or]: [
-          {
-            field1: {
-              [Sequelize.Op.iLike]: `%${searchAttributeString(search)}%`,
-            },
-          },
-          {
-            field2: {
-              [Sequelize.Op.iLike]: `%${searchAttributeString(search)}%`,
-            },
-          },
-        ],
-      }),
-      ...filters,
-    },
-    limit: parseInt(limit),
-    offset: parseInt(offset),
-    order: [[sortBy, sortOrder]],
-  }
-
-  const result = await examplesModel.findAndCountAll(options)
-
-  return {
-    count: result.count,
-    data: result.rows,
-  }
-}
-
-const findSubcategoryByID = async exampleID => {
-  return await examplesModel.findOne({
-    where: {
-      id: exampleID,
+      id: subcategoryID,
       deletedAt: null,
     },
   })
 }
 
 const findSubcategoryByName = async field1 => {
-  return await examplesModel.findOne({
+  return await subcategory.findOne({
     where: {
-      field1,
+       subcategory_name:{
+        [Op.like] : `%${field1}%`
+       },
       deletedAt: null,
     },
   })
 }
 
-const updateSubcategory = async (exampleID, data) => {
-  await examplesModel.update(data, {
+const updateSubcategory = async (subcategoryID, data) => {
+  await subcategory.update(data, {
     where: {
-      id: exampleID,
+      id: subcategoryID,
       deletedAt: null,
     },
   })
 
-  return await findExampleByID(exampleID)
+  return await findSubcategoryByID(subcategoryID)
 }
 
-const deleteSubcategory = async exampleID => {
-  return await examplesModel.destroy({
+const deleteSubcategory = async subcategoryID => {
+  return await subcategory.destroy({
     where: {
-      id: exampleID,
+      id: subcategoryID,
       deletedAt: null,
     },
   })
